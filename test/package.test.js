1366c107928013ec73050b6d4a551c68b5fcf74d
Jared Hanson <jaredhanson@gmail.com>
2017-10-05 15:53:40 -0700

    Implement.

--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,36 @@
-/* global describe, it */
-
-var pkg = require('..');
-var expect = require('chai').expect;
+var chai = require('chai');
+var mock = require('chai-kerouac-middleware');
+var cname = require('../lib');
 
 
 describe('kerouac-cname', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export function', function() {
+    expect(cname).to.be.a('function');
   });
+  
+  describe('without base url setting', function() {
+    var page, err;
+
+    before(function(done) {
+      chai.kerouac.use(cname())
+        .page(function(page) {
+          page.site = new mock.Site();
+          page.pages = [
+            { url: '/hello' },
+          ];
+        })
+        .next(function(e) {
+          err = e;
+          done();
+        })
+        .dispatch();
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('CNAME require "base url" setting');
+    });
+  }); // without base url setting
   
 });
